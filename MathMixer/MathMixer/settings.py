aa27b411d4594774d5f4b15ecec81ac7a8dc4ddd
OverLord434 <2005ramzes@gmail.com>
2024-09-30 16:35:51 +0300

    //

---
+++ MathMixer/MathMixer/settings.py
@@ -0,0 +1,107 @@
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = "django-insecure-$qnmjf7l6i)%^4rvtlg=%=_4j=11i!31n*=__^%8gm+%o-67_="
+
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    'rest_framework',
+    'mainapp',
+    'corsheaders',
+    'usersapp'
+]
+
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+ROOT_URLCONF = "MathMixer.urls"
+
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+
+WSGI_APPLICATION = "MathMixer.wsgi.application"
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Math_db',
+        'USER': 'postgres',
+        'PASSWORD': 'kalina2005',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+STATIC_URL = "static/"
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
---
+++ MathMixer/frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Usernames</h1>
+    <ul>
+      <li v-for="user in users" :key="user.username">{{ user.username }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+
+  created() {
+    axios.get('http://localhost:8000/api/users/')
+      .then(response => {
+        this.users = response.data
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+}
+</script>
